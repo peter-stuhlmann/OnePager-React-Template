a703eb528bb43698f541e7ee73e304b693c03e5c
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2021-10-22 00:41:48 +0200

    Add loading message styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Suspense } from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import styled from 'styled-components';
 
 import './assets/css/global.css';
 
@@ -8,9 +9,18 @@ import Router from './components/Router';
 export default function App() {
   return (
     <BrowserRouter>
-      <Suspense fallback={'Loading...'}>
+      <Suspense fallback={<Loading>Loading...</Loading>}>
         <Router />
       </Suspense>
     </BrowserRouter>
   );
 }
+
+const Loading = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-transform: uppercase;
+`;
