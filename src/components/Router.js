f2504b188e2f8aa7516c78b8aff529bee7768693
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2021-10-23 14:52:39 +0200

    On Error 404: Render Home component instead of NotFound component

--- src/components/Router.js
+++ src/components/Router.js
@@ -2,13 +2,14 @@ import React, { lazy } from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 const Home = lazy(() => import('./Home'));
-const NotFound = lazy(() => import('./NotFound'));
+// const NotFound = lazy(() => import('./NotFound'));
 
 export default function Router() {
   return (
     <Switch>
       <Route exact path="/" component={Home} />
-      <Route component={NotFound} />
+      {/* <Route component={NotFound} /> */}
+      <Route component={Home} />
     </Switch>
   );
 }
